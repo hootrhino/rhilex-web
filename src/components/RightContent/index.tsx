d6294ee96e50cc436758780fbc0a5339838705fe
Evey-huang <eveywong17@icloud.com>
2023-03-10 17:40:47 +0800

    chore: update config

--- src/components/RightContent/index.tsx
+++ src/components/RightContent/index.tsx
@@ -16,40 +16,9 @@ const GlobalHeaderRight: React.FC = () => {
     };
   });
 
-  // const actionClassName = useEmotionCss(({ token }) => {
-  //   return {
-  //     display: 'flex',
-  //     float: 'right',
-  //     height: '48px',
-  //     marginLeft: 'auto',
-  //     overflow: 'hidden',
-  //     cursor: 'pointer',
-  //     padding: '0 12px',
-  //     borderRadius: token.borderRadius,
-  //     '&:hover': {
-  //       backgroundColor: token.colorBgTextHover,
-  //     },
-  //   };
-  // });
-
-  // const { initialState } = useModel('@@initialState');
-
-  // if (!initialState || !initialState.settings) {
-  //   return null;
-  // }
-
   return (
     <div className={className}>
-      {/* <span
-        className={actionClassName}
-        onClick={() => {
-          window.open('https://pro.ant.design/docs/getting-started');
-        }}
-      >
-        <QuestionCircleOutlined />
-      </span> */}
       <Avatar />
-      {/* <SelectLang className={actionClassName} /> */}
     </div>
   );
 };
