d9bf2fc3fa3908d9698c24bab9b1ff56422c722a
Evey-huang <eveywong17@icloud.com>
2023-06-06 11:01:48 +0800

    feat: 完成插件管理-下线功能

--- src/pages/Plugins/Detail/index.tsx
+++ src/pages/Plugins/Detail/index.tsx
@@ -1,18 +1,28 @@
-import type { ProColumns } from '@ant-design/pro-components';
+import type { ActionType, ProColumns } from '@ant-design/pro-components';
 import { PageContainer, ProTable } from '@ant-design/pro-components';
 
+import { message } from '@/components/PopupHack';
 import { postPluginService } from '@/services/rulex/chajianguanli';
 import { CheckCircleOutlined, CloseCircleOutlined } from '@ant-design/icons';
 import { Tag } from 'antd';
+import { useRef } from 'react';
 import { useParams } from 'umi';
 
 type Item = Record<string, any>;
 
 const Detail = () => {
-  const { id } = useParams();
+  const { id: uuid } = useParams();
+  const actionRef = useRef<ActionType>();
 
-  const handleOffLine = () => {
-    // TODO
+  const handleOffLine = async (ids: string[]) => {
+    try {
+      await postPluginService({ uuid: uuid || '', name: 'kickout', args: ids });
+      message.success('下线成功');
+      actionRef.current?.reload();
+      return true;
+    } catch (error) {
+      return false;
+    }
   };
 
   const columns: ProColumns<Item>[] = [
@@ -47,8 +57,8 @@ const Detail = () => {
       width: 80,
       fixed: 'right',
       key: 'option',
-      render: () => [
-        <a key="offline" onClick={() => handleOffLine()}>
+      render: (_, { id }) => [
+        <a key="offline" onClick={() => handleOffLine([id])}>
           下线
         </a>,
       ],
@@ -59,9 +69,10 @@ const Detail = () => {
     <PageContainer>
       <ProTable
         rowKey="id"
+        actionRef={actionRef}
         columns={columns}
         request={async () => {
-          const { data } = await postPluginService({ uuid: id || '', name: 'clients' });
+          const { data } = await postPluginService({ uuid: uuid || '', name: 'clients', args: [] });
 
           return Promise.resolve({
             data,
