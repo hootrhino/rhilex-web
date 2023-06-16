960f4e1635a59734ccc158df847d3e8cb2b43cce
Evey-huang <eveywong17@icloud.com>
2023-06-16 14:02:40 +0800

    chore: 首页请求改为轮询

--- src/models/useWebsocket.ts
+++ src/models/useWebsocket.ts
@@ -1,11 +1,9 @@
-// import { useModel } from '@umijs/max';
 import { useWebSocket } from 'ahooks';
 import orderBy from 'lodash/orderBy';
 import slice from 'lodash/slice';
 import { useEffect, useState } from 'react';
 
 const useWebsocket = () => {
-  // const { data } = useModel('useSystem');
   const [logs, setLogs] = useState<Record<string, any>[]>([]);
   const [connected, setConnected] = useState(false);
   const [sockUrl, setUrl] = useState<string>('');
@@ -37,12 +35,6 @@ const useWebsocket = () => {
       }, 2000);
     }
   }, [connected, sendMessage]);
-
-  // useEffect(() => {
-  //   if (data?.hardWareInfo && data?.hardWareInfo?.wsUrl && data?.hardWareInfo?.wsUrl?.length > 0) {
-  //     setUrl(data?.hardWareInfo?.wsUrl?.[0]);
-  //   }
-  // }, [data]);
 
   useEffect(() => {
     if (window?.location?.hostname) {
