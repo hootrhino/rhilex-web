ec0f89bc2bba7e1bf5329d2222b00e69f412ab28
Evey-huang <eveywong17@icloud.com>
2023-06-16 15:41:37 +0800

    chore: loading.js 移动位置

--- config/config.ts
+++ config/config.ts
@@ -1,11 +1,11 @@
 // https://umijs.org/config/
 import { defineConfig } from '@umijs/max';
+import zhCN from 'antd/locale/zh_CN';
 import { join } from 'path';
 import defaultSettings from './defaultSettings';
 import proxy from './proxy';
 import routes from './routes';
 import theme from './theme';
-import zhCN from 'antd/locale/zh_CN';
 
 const { REACT_APP_ENV = 'dev' } = process.env;
 
@@ -123,7 +123,7 @@ export default defineConfig({
    */
   headScripts: [
     // 解决首次加载时白屏的问题
-    { src: '/scripts/loading.js', async: true },
+    { src: './loading.js', async: true },
   ],
   //================ pro 插件配置 =================
   presets: ['umi-presets-pro'],
