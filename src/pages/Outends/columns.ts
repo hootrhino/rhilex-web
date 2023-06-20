ac16a9b8a7c0f814e87a3ac941384623ee8aae98
Evey-huang <eveywong17@icloud.com>
2023-06-20 09:50:00 +0800

    feat: 目标管理新增MQTT&UDP类型

--- src/pages/Outends/columns.ts
+++ src/pages/Outends/columns.ts
@@ -14,6 +14,8 @@ export const columns = [
         required: true,
         valueEnum: {
           MONGO_SINGLE: 'MongoDB',
+          MQTT: 'MQTT Broker',
+          UDP_TARGET: 'UDP Server',
         },
       },
       {
@@ -53,6 +55,108 @@ export const columns = [
                       {
                         title: 'MongoDB 集合',
                         dataIndex: 'collection',
+                        required: true,
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+        ];
+      }
+      if (type === 'MQTT') {
+        return [
+          {
+            title: '目标配置',
+            valueType: 'group',
+            columns: [
+              {
+                valueType: 'formList',
+                dataIndex: ['config'],
+                mode: 'single',
+                columns: [
+                  {
+                    valueType: 'group',
+                    columns: [
+                      {
+                        title: '服务地址',
+                        dataIndex: 'host',
+                        required: true,
+                      },
+                      {
+                        title: '服务端口',
+                        dataIndex: 'port',
+                        valueType: 'digit',
+                        required: true,
+                      },
+                      {
+                        title: '客户端ID',
+                        dataIndex: 'clientId',
+                        required: true,
+                      },
+                    ],
+                  },
+                  {
+                    valueType: 'group',
+                    columns: [
+                      {
+                        title: '连接账户',
+                        dataIndex: 'username',
+                        required: true,
+                      },
+                      {
+                        title: '连接密码',
+                        dataIndex: 'password',
+                        valueType: 'password',
+                        required: true,
+                      },
+                      {
+                        title: '上报 TOPIC',
+                        dataIndex: 'pubTopic',
+                        required: true,
+                      },
+                    ],
+                  },
+                  {
+                    valueType: 'group',
+                    columns: [
+                      {
+                        title: '订阅 TOPIC',
+                        dataIndex: 'subTopic',
+                        required: true,
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+        ];
+      }
+      if (type === 'UDP_TARGET') {
+        return [
+          {
+            title: '目标配置',
+            valueType: 'group',
+            columns: [
+              {
+                valueType: 'formList',
+                dataIndex: ['config'],
+                mode: 'single',
+                columns: [
+                  {
+                    valueType: 'group',
+                    columns: [
+                      {
+                        title: '服务地址',
+                        dataIndex: 'host',
+                        required: true,
+                      },
+                      {
+                        title: '服务端口',
+                        dataIndex: 'port',
+                        valueType: 'digit',
                         required: true,
                       },
                     ],
