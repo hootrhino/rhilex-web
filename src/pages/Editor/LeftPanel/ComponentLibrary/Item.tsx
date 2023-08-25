fd799067b551dad9357b4599e455529635b5e48b
Evey-huang <eveywong17@icloud.com>
2023-08-25 10:31:22 +0800

    chore: 优化代码

---
+++ src/pages/Editor/LeftPanel/ComponentLibrary/Item.tsx
@@ -0,0 +1,43 @@
+import { IconFont } from '@/utils/utils';
+import { Tooltip } from 'antd';
+
+export type Data = {
+  title: string;
+  image: any;
+  key: string;
+  disabled?: boolean;
+};
+
+type ItemProps = {
+  data: Data;
+};
+
+const Item = ({ data }: ItemProps) => {
+  return (
+    <li className="bg-[#242424] mb-[12px] rounded-[4px] cursor-pointer" key={data.key}>
+      <div className="h-[24px] text-[12px] w-full px-[8px] overflow-hidden leading-[24px] text-[#7a7a7a]">
+        {data.title}
+      </div>
+      <div className="pt-0 pb-[4px] px-[8px] w-full h-[94px] relative">
+        <img
+          src={data.image}
+          alt={data.title}
+          className="w-full h-full object-cover cursor-pointer"
+        />
+        {data?.disabled && (
+          <span className="bg-black bg-opacity-60 w-full h-full text-center cursor-not-allowed absolute top-0 left-0 flex items-center justify-center">
+            <Tooltip
+              title="需要升级才可以使用当前组件包"
+              color="#4281ff"
+              overlayInnerStyle={{ fontSize: 10, borderRadius: 4 }}
+            >
+              <IconFont type="icon-lock" className="text-[16px]" />
+            </Tooltip>
+          </span>
+        )}
+      </div>
+    </li>
+  );
+};
+
+export default Item;
--- src/pages/Editor/LeftPanel/ComponentLibrary/index.tsx
+++ src/pages/Editor/LeftPanel/ComponentLibrary/index.tsx
@@ -1,15 +1,70 @@
 import { cn, IconFont } from '@/utils/utils';
 import { Tooltip } from 'antd';
-import { useState } from 'react';
-import { tabList } from '../constant';
-
-import Charts from './Charts';
-import Others from './Others';
+import { useEffect, useState } from 'react';
+import {
+  chartsList,
+  chartsTypeList,
+  mapList,
+  mediaList,
+  tableList,
+  tabList,
+  textList,
+  widgetList,
+} from '../constant';
+import ComponentItem from './Item';
+import type { Data } from './Item';
 
 import '../index.less';
 
 const ComponentLibrary = () => {
   const [activeTab, setTab] = useState<string>('charts');
+  const [activeType, setType] = useState<string>('all');
+  const [data, setData] = useState<Data[]>([]);
+
+  const getData = () => {
+    let dataSource: Data[] = [];
+
+    switch (activeTab) {
+      case 'charts':
+        chartsList?.forEach((item) => dataSource.push(...item.children));
+        break;
+      case 'map':
+        dataSource = [...mapList];
+        break;
+      case 'text':
+        dataSource = [...textList];
+        break;
+      case 'table':
+        dataSource = [...tableList];
+        break;
+      case 'widget':
+        dataSource = [...widgetList];
+        break;
+      case 'media':
+        dataSource = [...mediaList];
+        break;
+      default:
+        dataSource = [];
+    }
+
+    setData(dataSource);
+  };
+
+  useEffect(() => {
+    getData();
+  }, [activeTab]);
+
+  useEffect(() => {
+    if (activeType === 'all') {
+      const allCharts: Data[] = [];
+      chartsList?.forEach((item) => allCharts.push(...item.children));
+
+      setData(allCharts);
+    } else {
+      const currentCharts = chartsList?.find((item) => item.group === activeType);
+      setData(currentCharts?.children as Data[]);
+    }
+  }, [activeType]);
 
   return (
     <div className="flex flex-1 h-full w-full bg-[#1A1A1A]">
@@ -36,7 +91,36 @@ const ComponentLibrary = () => {
           </div>
         ))}
       </div>
-      {activeTab === 'charts' ? <Charts /> : <Others activeTab={activeTab} />}
+      {activeTab === 'charts' ? (
+        <div className="w-full flex flex-1">
+          <div className="flex flex-col py-[12px] ml-[8px] items-center">
+            {chartsTypeList?.map((item) => (
+              <div
+                key={item.key}
+                className={cn(
+                  'w-[50px] my-[10px] text-center rounded-[4px] p-[4px] bg-transparent text-[#7A7A7A] truncate cursor-pointer',
+                  activeType === item.key && 'bg-[#333] text-[#DBDBDB]',
+                )}
+                onClick={() => setType(item.key)}
+              >
+                {item.value}
+              </div>
+            ))}
+          </div>
+
+          <ul className={cn('charts-wrapper', 'custom-scrollbar', 'p-[12px]')}>
+            {data?.map((chart) => (
+              <ComponentItem data={chart} key={chart.key} />
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <ul className={cn('charts-wrapper', 'custom-scrollbar', 'p-[18px] w-full')}>
+          {data?.map((item) => (
+            <ComponentItem data={item} key={item.key} />
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
