765cb0f22b339a892249d039e87afd3b1ad7d170
Evey-huang <eveywong17@icloud.com>
2023-09-10 17:11:10 +0800

    chore: 更新协议文件

--- src/services/rulex/xitongshuju.ts
+++ src/services/rulex/xitongshuju.ts
@@ -2,6 +2,38 @@
 /* eslint-disable */
 import { request } from '@umijs/max';
 
+/** 网卡列表 GET /api/v1/os/netInterfaces */
+export async function getOsNetInterfaces(options?: { [key: string]: any }) {
+  return request<Record<string, any>>('/api/v1/os/netInterfaces', {
+    method: 'GET',
+    ...(options || {}),
+  });
+}
+
+/** 系统发行 GET /api/v1/os/osRelease */
+export async function getOsOsRelease(options?: { [key: string]: any }) {
+  return request<Record<string, any>>('/api/v1/os/osRelease', {
+    method: 'GET',
+    ...(options || {}),
+  });
+}
+
+/** 开机时间 GET /api/v1/os/startedAt */
+export async function getOsStartedAt(options?: { [key: string]: any }) {
+  return request<Record<string, any>>('/api/v1/os/startedAt', {
+    method: 'GET',
+    ...(options || {}),
+  });
+}
+
+/** 串口列表 GET /api/v1/os/uarts */
+export async function getOsUarts(options?: { [key: string]: any }) {
+  return request<Record<string, any>>('/api/v1/os/uarts', {
+    method: 'GET',
+    ...(options || {}),
+  });
+}
+
 /** 系统参数 获取首页dashboard数据 GET /api/v1/system */
 export async function getSystem(options?: { [key: string]: any }) {
   return request<{
@@ -24,14 +56,6 @@ export async function getSystem(options?: { [key: string]: any }) {
       statistic: { inSuccess?: number; outSuccess?: number; inFailed?: number; outFailed?: number };
     };
   }>('/api/v1/system', {
-    method: 'GET',
-    ...(options || {}),
-  });
-}
-
-/** 串口列表 GET /api/v1/uarts */
-export async function getUarts(options?: { [key: string]: any }) {
-  return request<Record<string, any>>('/api/v1/uarts', {
     method: 'GET',
     ...(options || {}),
   });
