98c769242a9107643a1bf8caa5c69797c604323d
Evey-huang <eveywong17@icloud.com>
2023-10-24 10:39:26 +0800

    chore: 完成网卡配置接口对接

--- src/pages/System/components/Network.tsx
+++ src/pages/System/components/Network.tsx
@@ -1,7 +1,8 @@
-import { getSettingsEth } from '@/services/rulex/wangluopeizhi';
-import { validateGateway, validateIPv4, validateMask } from '@/utils/utils';
+import { message } from '@/components/PopupHack';
+import { getSettingsEth, postSettingsEth } from '@/services/rulex/wangluopeizhi';
+import { validateGateway, validateIPv4 } from '@/utils/utils';
 import { PlusCircleOutlined } from '@ant-design/icons';
-import type { ProFormInstance } from '@ant-design/pro-components';
+import type { FormListActionType, ProFormInstance } from '@ant-design/pro-components';
 import {
   ProForm,
   ProFormList,
@@ -10,21 +11,59 @@ import {
   ProFormText,
 } from '@ant-design/pro-components';
 import { useRequest } from '@umijs/max';
-import { Space, Tooltip } from 'antd';
+import { Tooltip } from 'antd';
+import { omit } from 'lodash';
 import { useRef } from 'react';
+
+type DnsListItem = {
+  dns: string;
+};
+
+type BaseFormItem = {
+  interface: string;
+  address: string;
+  netmask: string;
+  gateway: string;
+  dhcp_enabled: boolean;
+  dnsList: DnsListItem[];
+};
+
+type UpdateParams = Omit<BaseFormItem, 'dnsList'> & {
+  dns: string[];
+};
 
 const NetworkConfig = () => {
   const formRef = useRef<ProFormInstance>();
+  const actionRef = useRef<FormListActionType>();
 
-  // TODO 详情
-  const {data: detail} = useRequest(() => getSettingsEth());
+  // 详情
+  const { data: detail } = useRequest(() => getSettingsEth(), {
+    onSuccess: (data) => {
+      if (!data) {
+        formRef.current?.setFieldsValue({
+          interface: 'eth0',
+          address: '192.168.199.1',
+          netmask: '255.255.255.0',
+          gateway: '192.168.199.1',
+          dnsList: [{ dns: '8.8.8.8' }, { dns: '114.114.114.114' }],
+        });
+      } else {
+        const dnsList = data['eth0'].dns?.map((item) => ({ dns: item }));
+        formRef.current?.setFieldsValue({ ...omit(data['eth0'], 'dns'), dnsList });
+      }
+    },
+  });
 
-  console.log(detail);
-
-  // TODO 更新
-  const handleOnFinish = async (values) => {
+  // 更新
+  const handleOnFinish = async ({ dnsList, ...values }: BaseFormItem) => {
     try {
-      console.log(values);
+      const params = {
+        ...values,
+        dns: dnsList?.map((item: { dns: string }) => item.dns),
+      };
+      await postSettingsEth(params as UpdateParams);
+      message.success('修改成功');
+
       return true;
     } catch (error) {
       return false;
@@ -39,9 +78,11 @@ const NetworkConfig = () => {
         onFinish={handleOnFinish}
         layout="horizontal"
         labelCol={{ span: 2 }}
-        initialValues={{ interface: 'eth0', address: '192.168.199.1', mask: '255.255.255.0', gateway: '192.168.199.1' }}
-        submitter={{
-          render: (props, dom) => <Space className="flex justify-end">{dom}</Space>,
+        onValuesChange={(changedValue) => {
+          console.log(changedValue);
+          if (changedValue?.interface && detail) {
+            formRef.current?.setFieldsValue({ ...detail[changedValue?.interface] });
+          }
         }}
       >
         <ProFormSelect
@@ -53,6 +94,7 @@ const NetworkConfig = () => {
           label="网卡选择"
           width="xl"
           placeholder="请选择网卡"
+          rules={[{ required: true, message: '请选择网卡' }]}
         />
         <ProFormText
           name="address"
@@ -60,6 +102,7 @@ const NetworkConfig = () => {
           width="xl"
           placeholder="请输入 IP 地址"
           rules={[
+            { required: true, message: '请输入 IP 地址' },
             () => ({
               validator(_, value) {
                 if (!value) {
@@ -80,28 +123,29 @@ const NetworkConfig = () => {
           ]}
         />
         <ProFormText
-          name="mask"
+          name="netmask"
           label="子网掩码"
           width="xl"
           placeholder="请输入子网掩码"
           rules={[
-            () => ({
-              validator(_, value) {
-                if (!value) {
-                  return Promise.resolve();
-                } else {
-                  try {
-                    if (!validateMask(value)) {
-                      return Promise.reject(new Error('子网掩码格式不正确，请检查'));
-                    } else {
-                      return Promise.resolve();
-                    }
-                  } catch (error) {
-                    return Promise.reject(new Error('子网掩码格式不正确，请检查'));
-                  }
-                }
-              },
-            }),
+            { required: true, message: '请输入子网掩码' },
+            // () => ({
+            //   validator(_, value) {
+            //     if (!value) {
+            //       return Promise.resolve();
+            //     } else {
+            //       try {
+            //         if (!validateMask(value)) {
+            //           return Promise.reject(new Error('子网掩码格式不正确，请检查'));
+            //         } else {
+            //           return Promise.resolve();
+            //         }
+            //       } catch (error) {
+            //         return Promise.reject(new Error('子网掩码格式不正确，请检查'));
+            //       }
+            //     }
+            //   },
+            // }),
           ]}
         />
         <ProFormText
@@ -110,6 +154,7 @@ const NetworkConfig = () => {
           width="xl"
           placeholder="请输入默认网关"
           rules={[
+            { required: true, message: '请输入默认网关' },
             () => ({
               validator(_, value) {
                 if (!value) {
@@ -131,17 +176,10 @@ const NetworkConfig = () => {
         />
 
         <ProFormList
+          actionRef={actionRef}
           name="dnsList"
           label="DNS服务器"
           min={1}
-          initialValue={[
-            {
-              dns: '8.8.8.8',
-            },
-            {
-              dns: '114.114.114.1114',
-            },
-          ]}
           creatorButtonProps={false}
           creatorRecord={{
             dns: '',
@@ -154,14 +192,21 @@ const NetworkConfig = () => {
               ...defaultActionDom,
             ];
           }}
+          required
         >
-          <ProFormText name='dns' width="xl" placeholder="请输入DNS服务器" />
+          <ProFormText
+            name="dns"
+            width="xl"
+            placeholder="请输入DNS服务器"
+            rules={[{ required: true, message: '请输入DNS服务器' }]}
+          />
         </ProFormList>
         <ProFormSwitch
           name="dhcp_enabled"
           label="开启DHCP"
           checkedChildren="开启"
           unCheckedChildren="关闭"
+          rules={[{ required: true }]}
         />
       </ProForm>
     </>
