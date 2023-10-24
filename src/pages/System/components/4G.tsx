8b29f92ee70787c47b29d4012207b29096806b32
Evey-huang <eveywong17@icloud.com>
2023-10-24 11:23:52 +0800

    chore: 4G配置接口对接

--- src/pages/System/components/4G.tsx
+++ src/pages/System/components/4G.tsx
@@ -1,33 +1,42 @@
+import { message } from '@/components/PopupHack';
+import { get4gInfo, postSettings4gRestart } from '@/services/rulex/4Gshezhi';
 import type { ProFormInstance } from '@ant-design/pro-components';
 import { ProForm, ProFormText } from '@ant-design/pro-components';
+import { useRequest } from '@umijs/max';
 import { Button, Progress, Space } from 'antd';
 import { useRef } from 'react';
 
 const FourGConfig = () => {
   const formRef = useRef<ProFormInstance>();
 
-  const handleOnFinish = async (values) => {
-    console.log(values);
-    try {
-      return true;
-    } catch (error) {
-      return false;
-    }
-  };
+  // TODO 详情
+  useRequest(() => get4gInfo(), {
+    onSuccess: (data) => {
+      // TODO 返回参数不明确
+      console.log(data);
+    },
+  });
+
+  // 重启4G
+  const { run: restart } = useRequest(() => postSettings4gRestart(), {
+    manual: true,
+    onSuccess: () => message.success('重启成功'),
+  });
 
   return (
     <>
-      <div className="text-[20px] mb-[24px] font-medium">4G配置</div>
+      <div className="text-[20px] mb-[24px] font-medium">4G 配置</div>
       <ProForm
         formRef={formRef}
-        onFinish={handleOnFinish}
         layout="horizontal"
         labelCol={{ span: 2 }}
         initialValues={{ zone: 'Asia/Shanghai' }}
         submitter={{
           render: () => (
-            <Space className="flex justify-end">
-              <Button type="primary">重启4G网卡</Button>
+            <Space>
+              <Button type="primary" onClick={restart}>
+                重启 4G 网卡
+              </Button>
             </Space>
           ),
         }}
