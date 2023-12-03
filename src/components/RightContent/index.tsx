ef1b12b06e6a452d3d52cee46ef455bd92d439b0
Evey-huang <eveywong17@icloud.com>
2023-12-03 14:57:14 +0800

    feat: 新增github图标

--- src/components/RightContent/index.tsx
+++ src/components/RightContent/index.tsx
@@ -1,3 +1,4 @@
+import { GithubFilled } from '@ant-design/icons';
 import { useModel } from '@umijs/max';
 import Avatar from './AvatarDropdown';
 
@@ -10,6 +11,24 @@ const GlobalHeaderRight = () => {
     return null;
   }
 
-  return <Avatar />;
+  return (
+    <div className="flex justify-end items-center">
+      <GithubFilled
+        style={{
+          fontSize: 24,
+          color: 'rgba(255, 255, 255, 0.95)',
+          height: 56,
+          cursor: 'pointer',
+          padding: '0 8px',
+        }}
+        onClick={() => {
+          const w = window.open('about:blank');
+          if (!w) return;
+          w.location.href = 'https://github.com/hootrhino';
+        }}
+      />
+      <Avatar />
+    </div>
+  );
 };
 export default GlobalHeaderRight;
