5b1130eae2685d6633dcb95037db12e3ab5fecab
Evey-huang <eveywong17@icloud.com>
2024-01-29 23:59:55 +0800

    chore: 完成设备-播放视频功能

--- src/pages/Devices/VideoDetail/index.tsx
+++ src/pages/Devices/VideoDetail/index.tsx
@@ -1,71 +1,54 @@
-import { useWebSocket } from 'ahooks';
 import type { ModalProps } from 'antd';
 import { Button, Modal } from 'antd';
 import CryptoJS from 'crypto-js';
-import { useEffect, useRef } from 'react';
-import flvjs from 'flv.js';
+import mpegts from 'mpegts.js';
+import { useRef } from 'react';
 
 type VideoDetailProps = ModalProps & {
   liveUrl: string;
 };
-
+// TODO ws建立连接&销毁优化
 const VideoDetail = ({ onCancel, liveUrl, ...props }: VideoDetailProps) => {
   const videoRef = useRef(null);
+  const playerRef = useRef(null);
+  const hash = liveUrl && CryptoJS.MD5(liveUrl);
 
-//   const mediaSourceRef = useRef(null);
-//   const sourceBufferRef = useRef(null);
-
-//   const hash = liveUrl && CryptoJS.MD5(liveUrl);
-
-//   const { sendMessage, readyState, latestMessage, disconnect } = useWebSocket(
-//     `ws://106.15.225.172:9400/ws?token=WebRtspPlayer&liveId=123a4509de8a99aa0f680cb5847e0765`,
-//   );
-
-//   const handleUpdateEnd = () => {
-//     if (!sourceBufferRef.current.updating && mediaSourceRef.current.readyState === 'open') {
-//       mediaSourceRef.current.endOfStream();
-//     }
-//   };
-
-//   useEffect(() => {
-//     if (videoRef.current) {
-//       console.log(mediaSourceRef.current);
-//       const handleSourceOpen = () => {
-//       mediaSourceRef.current.removeEventListener('sourceopen', handleSourceOpen);
-// if (mediaSourceRef.current.readyState === 'open') {
-//   const sourceBuffer = mediaSourceRef.current?.addSourceBuffer('video/mp4; codecs="avc1.42E01E, mp4a.40.2"');
-//   sourceBufferRef.current = sourceBuffer;
-//   sourceBuffer.addEventListener('updateend', handleUpdateEnd);
-// }
-
-//     };
-
-//     const mediaSource = new MediaSource();
-//     mediaSourceRef.current = mediaSource;
-//     videoRef.current.src = URL.createObjectURL(mediaSource);
-
-//     mediaSource.addEventListener('sourceopen', handleSourceOpen);
-//     }
-//   }, [latestMessage?.data]);
+  if (mpegts.getFeatureList().mseLivePlayback) {
+    const player = mpegts.createPlayer(
+      {
+        type: 'flv',
+        isLive: true,
+        url: `ws://${window?.location?.host}/ws?token=WebRtspPlayer&liveId=${hash}`,
+        // url: 'ws://106.15.225.172:9400/ws?token=WebRtspPlayer&liveId=123a4509de8a99aa0f680cb5847e0765'
+      },
+      { enableWorker: true },
+    );
+    playerRef.current = player;
+    if (videoRef.current) {
+      player.attachMediaElement(videoRef.current);
+      player.load();
+      player.play();
+    }
+  }
 
   return (
     <Modal
       destroyOnClose
       title="查看视频"
       width="50%"
-      onCancel={onCancel}
+      onCancel={() => {
+        onCancel();
+        playerRef.current?.destroy();
+      }}
       maskClosable={false}
       footer={
-        <Button
-          type="primary"
-          onClick={onCancel}
-        >
+        <Button type="primary" onClick={onCancel}>
           关闭
         </Button>
       }
       {...props}
     >
-      <video controls width="100%" id='videoElement' ref={videoRef}></video>
+      <video controls width="100%" id="videoElement" ref={videoRef}></video>
     </Modal>
   );
 };
