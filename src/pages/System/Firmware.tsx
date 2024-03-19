0d063ac2b2cf3166d917e0deda264ab89b3ba1f8
Evey-huang <eveywong17@icloud.com>
2024-03-19 17:47:37 +0800

    chore: 优化系统配置-固件升级用户体验&代码

--- src/pages/System/Firmware.tsx
+++ src/pages/System/Firmware.tsx
@@ -10,19 +10,17 @@ import {
   postFirmwareUpload,
 } from '@/services/rulex/gujiancaozuo';
 import { IconFont } from '@/utils/utils';
-import { CloudUploadOutlined, PoweroffOutlined, SyncOutlined } from '@ant-design/icons';
-import type { ProFormInstance } from '@ant-design/pro-components';
 import {
-  ProCard,
-  ProForm,
-  ProFormTextArea,
-  ProFormUploadDragger,
-} from '@ant-design/pro-components';
+  CloudUploadOutlined,
+  PoweroffOutlined,
+  SyncOutlined,
+  UploadOutlined,
+} from '@ant-design/icons';
+import { ProCard, ProDescriptions } from '@ant-design/pro-components';
 import { useModel, useRequest } from '@umijs/max';
-import { Button, Space } from 'antd';
-import type { UploadFile } from 'antd/es/upload/interface';
+import { Button, Modal, Progress, Space, Upload } from 'antd';
 import endsWith from 'lodash/endsWith';
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import Title from './components/Title';
 
 type ConfirmCofig = {
@@ -56,26 +54,27 @@ const defaultRecoverConfig = {
 };
 
 const FirmwareConfig = () => {
-  const formRef = useRef<ProFormInstance>();
+  // const formRef = useRef<ProFormInstance>();
   const { run, cancel } = useModel('useSystem');
   const [open, setOpen] = useState<boolean>(false);
   const [confirmConfig, setConfirmConfig] = useState<ConfirmCofig>(defaultUpgradeConfig);
   const [errorMsg, setMsg] = useState<string>('');
-  const [fileList, setFileList] = useState<UploadFile[]>([]);
+  const [showProgress, setShowProgress] = useState<boolean>(false);
+  const [uploadProgress, setProgress] = useState<number>(0);
 
-  const { run: getVendorKey } = useRequest(() => getFirmwareVendorKey(), {
-    onSuccess: (data: string) => formRef.current?.setFieldsValue({ vendorKey: data }),
-  });
+  // const { run: getVendorKey } = useRequest(() => getFirmwareVendorKey(), {
+  //   onSuccess: (data: string) => formRef.current?.setFieldsValue({ vendorKey: data }),
+  // });
 
   // 上传固件
   const { run: uploadFile } = useRequest((params) => postFirmwareUpload({}, params), {
     manual: true,
     onSuccess: () => {
+      setProgress(100);
       message.success('上传成功');
-      setFileList([{ ...fileList[0], status: 'done', percent: 100 }]);
       setTimeout(() => {
-        formRef.current?.setFieldsValue({ upload: [] });
-        setFileList([]);
+        setShowProgress(false);
+        setProgress(0);
       }, 500);
     },
   });
@@ -84,7 +83,7 @@ const FirmwareConfig = () => {
   const { data: logData } = useRequest(() => getFirmwareUpgradeLog());
 
   const handleOnEnd = () => {
-    getVendorKey();
+    // getVendorKey();
     setOpen(false);
     run();
     message.success(errorMsg ? errorMsg : `${confirmConfig.afterOkText}成功`);
@@ -95,139 +94,150 @@ const FirmwareConfig = () => {
       <Title name="固件升级" />
       <ProCard split="vertical">
         <ProCard colSpan="50%">
-          <ProForm
-            formRef={formRef}
-            layout="vertical"
-            submitter={{
-              render: () => (
-                <Space>
-                  <Button
-                    key="upgrade"
-                    type="primary"
-                    onClick={() => {
-                      setOpen(true);
-                      setConfirmConfig({
-                        ...defaultUpgradeConfig,
-                        handleOnOk: async () => {
-                          const { data } = await postFirmwareUpgrade();
-                          setMsg(data);
-                          cancel();
-                        },
-                        handleOnEnd,
-                      });
-                    }}
-                    icon={<CloudUploadOutlined />}
-                  >
-                    确定升级
-                  </Button>
-                  <Button
-                    key="reboot"
-                    type="primary"
-                    danger
-                    onClick={() => {
-                      setOpen(true);
-                      setConfirmConfig({
-                        ...defaultRebootConfig,
-                        handleOnOk: async () => {
-                          const { data } = await postFirmwareReboot();
-                          setMsg(data);
-                          cancel();
-                        },
-                        handleOnEnd,
-                      });
-                    }}
-                    icon={<PoweroffOutlined />}
-                  >
-                    重启设备
-                  </Button>
-                  <Button
-                    key="restartRulex"
-                    type="primary"
-                    className="flex items-center"
-                    danger
-                    onClick={() => {
-                      setOpen(true);
-                      setConfirmConfig({
-                        ...defaultRebootConfig,
-                        handleOnOk: async () => {
-                          const { data } = await postFirmwareRestartRulex();
-                          setMsg(data);
-                          cancel();
-                        },
-                        handleOnEnd,
-                      });
-                    }}
-                    icon={<IconFont type="icon-restart-rulex" className="text-[16px]" />}
-                  >
-                    重启固件
-                  </Button>
-                  <Button
-                    key="recover"
-                    type="primary"
-                    danger
-                    onClick={() => {
-                      setOpen(true);
-                      setConfirmConfig({
-                        ...defaultRecoverConfig,
-                        handleOnOk: async () => {
-                          const { data } = await postFirmwareRecoverNew();
-                          setMsg(data);
-                          cancel();
-                        },
-                        handleOnEnd,
-                      });
-                    }}
-                    icon={<SyncOutlined />}
-                  >
-                    恢复出厂
-                  </Button>
-                </Space>
-              ),
+          <ProDescriptions
+            column={1}
+            title={<div className="font-normal text-[14px]">设备授权信息</div>}
+            request={async () => {
+              const { data } = await getFirmwareVendorKey();
+              return Promise.resolve({
+                success: true,
+                data,
+              });
             }}
-            onValuesChange={async (changedValue) => {
-              if (changedValue?.upload) {
-                const file = changedValue?.upload?.[0];
+            labelStyle={{ justifyContent: 'flex-end', minWidth: 135 }}
+          >
+            <ProDescriptions.Item label="设备生产序列号" dataIndex="device_id" />
+            <ProDescriptions.Item label="设备硬件 MAC 地址" dataIndex="mac" />
+            <ProDescriptions.Item label="证书签发人" dataIndex="authorize_admin" />
+            <ProDescriptions.Item label="证书签发人密钥" dataIndex="authorize_password" />
+            <ProDescriptions.Item label="证书授权开始时间" dataIndex="begin_authorize" />
+            <ProDescriptions.Item label="证书授权结束时间" dataIndex="end_authorize" />
+            <ProDescriptions.Item label="私钥" dataIndex="private_key" />
+            <ProDescriptions.Item label="公钥" dataIndex="public_key" />
+          </ProDescriptions>
+
+          <Space className="mt-[30px]">
+            <Upload
+              accept="application/zip"
+              showUploadList={false}
+              beforeUpload={(file) => {
+                const isZip = file.type === 'application/zip' || endsWith(file?.name, '.zip');
+
+                if (!isZip) {
+                  message.error('仅支持 zip 格式文件，请检查上传文件格式');
+                }
+
+                return new Promise((resolve) => {
+                  resolve(file);
+                });
+              }}
+              onChange={(info) => {
+                const file = info.file;
 
                 if (file?.percent === 100) {
                   if (file?.status === 'done') {
                     uploadFile(file?.originFileObj);
                   }
-                  setFileList([{ ...file, percent: 99, status: 'uploading' }]);
+                  setProgress(99);
                 } else {
-                  setFileList(changedValue?.upload);
-                }
-              }
-            }}
-          >
-            <ProFormTextArea name="vendorKey" label="一机一密" placeholder="" disabled width="xl" />
-            <ProFormUploadDragger
-              label="固件上传"
-              name="upload"
-              max={1}
-              accept="application/zip"
-              description="仅支持 zip 格式文件"
-              fieldProps={{
-                style: { maxWidth: 550 },
-                fileList,
-                beforeUpload: (file) => {
-                  const isZip = file.type === 'application/zip' || endsWith(file?.name, '.zip');
-
-                  if (!isZip) {
-                    message.error('仅支持 zip 格式文件，请检查上传文件格式');
+                  if (file?.percent === 0) {
+                    setShowProgress(true);
                   }
-
-                  return new Promise((resolve) => {
-                    resolve(file);
-                  });
-                },
+                  setProgress(file?.percent || 0);
+                }
               }}
-              width="xl"
-            />
-          </ProForm>
+            >
+              <Button key="upload" type="primary" icon={<UploadOutlined />}>
+                上传固件
+              </Button>
+            </Upload>
+            <Button
+              key="upgrade"
+              type="primary"
+              onClick={() => {
+                setOpen(true);
+                setConfirmConfig({
+                  ...defaultUpgradeConfig,
+                  handleOnOk: async () => {
+                    const { data } = await postFirmwareUpgrade();
+                    setMsg(data);
+                    cancel();
+                  },
+                  handleOnEnd,
+                });
+              }}
+              icon={<CloudUploadOutlined />}
+            >
+              确定升级
+            </Button>
+            <Button
+              key="reboot"
+              type="primary"
+              danger
+              onClick={() => {
+                setOpen(true);
+                setConfirmConfig({
+                  ...defaultRebootConfig,
+                  handleOnOk: async () => {
+                    const { data } = await postFirmwareReboot();
+                    setMsg(data);
+                    cancel();
+                  },
+                  handleOnEnd,
+                });
+              }}
+              icon={<PoweroffOutlined />}
+            >
+              重启设备
+            </Button>
+            <Button
+              key="restartRulex"
+              type="primary"
+              className="flex items-center"
+              danger
+              onClick={() => {
+                setOpen(true);
+                setConfirmConfig({
+                  ...defaultRebootConfig,
+                  handleOnOk: async () => {
+                    const { data } = await postFirmwareRestartRulex();
+                    setMsg(data);
+                    cancel();
+                  },
+                  handleOnEnd,
+                });
+              }}
+              icon={<IconFont type="icon-restart-rulex" className="text-[16px]" />}
+            >
+              重启固件
+            </Button>
+            <Button
+              key="recover"
+              type="primary"
+              danger
+              onClick={() => {
+                setOpen(true);
+                setConfirmConfig({
+                  ...defaultRecoverConfig,
+                  handleOnOk: async () => {
+                    const { data } = await postFirmwareRecoverNew();
+                    setMsg(data);
+                    cancel();
+                  },
+                  handleOnEnd,
+                });
+              }}
+              icon={<SyncOutlined />}
+            >
+              恢复出厂
+            </Button>
+          </Space>
+          <Modal title="上传固件" open={showProgress} footer={false}>
+            <Progress percent={uploadProgress} size="small" />
+          </Modal>
         </ProCard>
         <ProCard title="固件升级日志" colSpan="50%">
           <div className="w-full break-words whitespace-pre">{logData}</div>
-          {/* <CodeEditor readOnly value={logData} className="w-full" theme="light" lang='shell' /> */}
         </ProCard>
       </ProCard>
       <ProConfirmModal open={open} onCancel={() => setOpen(false)} {...confirmConfig} />
