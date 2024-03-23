0eb1ae95dd30f03f46509159bf8bf10cac49d33e
Evey-huang <eveywong17@icloud.com>
2024-03-23 17:24:48 +0800

    fix: 修复esbuild

--- config/config.ts
+++ config/config.ts
@@ -35,6 +35,7 @@ export default defineConfig({
   },
   requestRecord: {},
   favicons: ['/favicon.png'],
+  esbuildMinifyIIFE: true,
   inlineLimit: 100000, // 配置图片文件是否走 base64 编译的阈值 100k
   // jsMinifier: 'uglifyJs', // 配置构建时压缩 JavaScript 的工具
   codeSplitting: { jsStrategy: 'granularChunks' }, // 代码拆分
