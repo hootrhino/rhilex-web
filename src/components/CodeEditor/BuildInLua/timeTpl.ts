1b830818031e30bf62b3433c93a06a665c18668b
Evey-huang <eveywong17@icloud.com>
2024-04-10 22:14:54 +0800

    chore: lua模板time函数名大写

--- src/components/CodeEditor/BuildInLua/timeTpl.ts
+++ src/components/CodeEditor/BuildInLua/timeTpl.ts
@@ -5,13 +5,13 @@
 import { firstUpperCase } from '@/utils/utils';
 
 const timeList = [
-  { target: 'time', detail: '当前时间' },
-  { target: 'timeMs', detail: '当前通用时间戳' },
-  { target: 'tsUnix', detail: '当前 Unix 时间戳' },
-  { target: 'tsUnixNano', detail: '当前纳秒级时间戳' },
-  { target: 'ntpTime', detail: 'NTP 时间' },
+  { target: 'Time', detail: '当前时间' },
+  { target: 'TimeMs', detail: '当前通用时间戳' },
+  { target: 'TsUnix', detail: '当前 Unix 时间戳' },
+  { target: 'TsUnixNano', detail: '当前纳秒级时间戳' },
+  { target: 'NtpTime', detail: 'NTP 时间' },
   {
-    target: 'sleep',
+    target: 'Sleep',
     detail: '休眠',
     variables: [{ label: '休眠时间', name: 'arg', value: 500, type: 'number' }],
   },
