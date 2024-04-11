8391b310892a578e43c2af94907f7f5b671a25fd
wwhai <cnwwhai@gmail.com>
2024-04-11 15:09:39 +0000

    ci: update ci build file

--- release.sh
+++ release.sh
@@ -1,4 +1,5 @@
-#! /bin/bash
+#!/bin/bash
+
 build() {
     if [ ! -d "./_release/" ]; then
         mkdir -p ./_release/
@@ -6,11 +7,16 @@ build() {
         rm -rf ./_release/
         mkdir -p ./_release/
     fi
-    yarn run build
-    zip -j $1.zip ./dist/*
-    mv ./$1.zip ./_release
+    npm install --global yarn
+    yarn install
+    if yarn run build; then
+        zip -j "www.zip" ./dist/* || { echo "Error: Failed to zip files."; exit 1; }
+        mv "./www.zip" ./_release || { echo "Error: Failed to move zip file to _release directory."; exit 1; }
+        echo "Build successful. Zip file is available in _release directory."
+    else
+        echo "Error: Failed to build project."
+        exit 1
+    fi
 }
-#--
-npm install --global yarn
-yarn
-build www
+
+build
