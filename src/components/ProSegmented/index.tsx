97582b573d04a89b4feb4d1616c360697ba88f72
Evey-huang <eveywong17@icloud.com>
2024-09-18 16:46:56 +0800

    chore: 优化 ProSegmented 组件

--- src/components/ProSegmented/index.tsx
+++ src/components/ProSegmented/index.tsx
@@ -1,62 +1,21 @@
+import type { ProFormFieldProps } from '@ant-design/pro-components';
+import { ProFormSegmented } from '@ant-design/pro-components';
 import { useIntl } from '@umijs/max';
-import type { SegmentedProps } from 'antd';
-import { ConfigProvider, Segmented } from 'antd';
 
-type ProSegmentedProps = Omit<SegmentedProps, 'options'> & {
-  width: 'sm' | 'md' | 'xl' | 'xs' | 'lg';
-};
+type ProSegmentedProps = ProFormFieldProps;
 
-const ProSegmented = ({ width, ...props }: ProSegmentedProps) => {
+const ProSegmented = (props: ProSegmentedProps) => {
   const { formatMessage } = useIntl();
 
-  const getWidth = () => {
-    let w = 328;
-
-    switch (width) {
-      case 'xs':
-        w = 104;
-        break;
-      case 'sm':
-        w = 216;
-        break;
-      case 'md':
-        w = 328;
-        break;
-      case 'xl':
-        w = 440;
-        break;
-      case 'lg':
-        w = 552;
-        break;
-      default:
-        w = 328;
-        break;
-    }
-
-    return w;
-  };
-
   return (
-    <ConfigProvider
-      theme={{
-        components: {
-          Segmented: {
-            itemSelectedColor: '#fff',
-            itemSelectedBg: '#1890ff',
-          },
-        },
+    <ProFormSegmented
+      valueEnum={{
+        true: formatMessage({ id: 'status.enabled' }),
+        false: formatMessage({ id: 'status.disabled' }),
       }}
-    >
-      <Segmented
-        block
-        style={{ width: getWidth() }}
-        options={[
-          { label: formatMessage({ id: 'status.enabled' }), value: 'true' },
-          { label: formatMessage({ id: 'status.disabled' }), value: 'false' },
-        ]}
-        {...props}
-      />
-    </ConfigProvider>
+      fieldProps={{ block: true, className: 'pro-form-segmented' }}
+      {...props}
+    />
   );
 };
 
