1bb906ccc17cfdba8a267cbe7ff4fff65eaeeea5
Evey-huang <eveywong17@icloud.com>
2024-10-12 20:25:38 +0800

    chore: 优化 RightContent 组件

--- src/components/HeaderDropdown/index.tsx
+++ src/components/HeaderDropdown/index.tsx
@@ -1,38 +1,45 @@
 import React from 'react';
 
-import { cn, IconFont } from '@/utils/utils';
-import { RightOutlined, UserOutlined } from '@ant-design/icons';
-import { history, useModel } from '@umijs/max';
-import type { DropDownProps } from 'antd';
-import { Avatar, Dropdown, Space, Tag, theme } from 'antd';
+import { IconFont } from '@/utils/utils';
+import {
+  ExclamationCircleOutlined,
+  LogoutOutlined,
+  RightOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+import { history, useIntl, useModel } from '@umijs/max';
+import type { PopoverProps } from 'antd';
+import { App, Avatar, Flex, Popover, Space, Tag } from 'antd';
 
-export type HeaderDropdownProps = {
-  overlayClassName?: string;
-  placement?: 'bottomLeft' | 'bottomRight' | 'topLeft' | 'topCenter' | 'topRight' | 'bottomCenter';
-} & Omit<DropDownProps, 'overlay'>;
-
-const HeaderDropdown: React.FC<HeaderDropdownProps> = ({ overlayClassName: cls, ...restProps }) => {
-  const { token } = theme.useToken();
+const HeaderDropdown: React.FC<PopoverProps> = (props) => {
+  const { modal } = App.useApp();
+  const { formatMessage } = useIntl();
+  const { logout } = useModel('useUser');
   const { setActiveKey } = useModel('useSystem');
   const { initialState } = useModel('@@initialState');
   const { currentUser } = initialState || {};
 
-  const contentStyle: React.CSSProperties = {
-    backgroundColor: token.colorBgElevated,
-    borderRadius: token.borderRadiusLG,
-    boxShadow: token.boxShadowSecondary,
-  };
-
-  const menuStyle: React.CSSProperties = {
-    boxShadow: 'none',
+  /**
+   * 退出登录，并且将当前的 url 保存
+   */
+  const loginOut = async () => {
+    modal.confirm({
+      title: formatMessage({ id: 'modal.content.logout' }),
+      icon: <ExclamationCircleOutlined />,
+      onOk: logout,
+      okText: formatMessage({ id: 'button.ok' }),
+      cancelText: formatMessage({ id: 'button.cancel' }),
+    });
   };
 
   return (
-    <Dropdown
-      overlayClassName={cn('xs:w-full', cls)}
-      dropdownRender={(menu) => (
-        <div style={contentStyle}>
-          <div className="flex items-center gap-[12px] pt-[24px] pb-[12px] pl-[18px] mx-[4px]">
+    <Popover
+      placement="bottomLeft"
+      arrow={false}
+      rootClassName="user-popover"
+      content={
+        <div className="w-[300px]">
+          <Flex align="center" gap={12} className="pt-[24px] pb-[12px] pl-[18px]">
             <Avatar
               size="large"
               className="text-[#fff] bg-[#e0eafc] mr-[8px] align-top md:m-0"
@@ -47,33 +54,33 @@ const HeaderDropdown: React.FC<HeaderDropdownProps> = ({ overlayClassName: cls, 
               </Space>
               <div className="text-[12px] text-[#1f212680] mt-[6px]">试用将于 2024.10.20 到期</div>
             </div>
-          </div>
+          </Flex>
           <div
-            className="rounded-[8px] mb-[10px] mx-[10px]"
+            className="rounded-[8px] mb-[10px] mx-[14px]"
             style={{ border: '1px solid rgba(5,5,5,0.06)' }}
           >
             <div className="gradient-grey-bg text-[#52565e] p-[12px] rounded-t-[8px]">
               <div className="text-[22px]">企业版</div>
               <div className="text-[12px] text-[rgba(5,5,5,0.5)]">尚未升级</div>
             </div>
-            <div className="mx-[10px] mt-[10px] mb-[20px]">
+            <div className="mx-[14px] mt-[10px] mb-[20px]">
               <div className="text-[rgba(5,5,5,0.7)] text-[12px]">升级企业版，立享专业服务</div>
-              <div className="flex items-center justify-center gap-[16px] my-[16px]">
-                <div className="flex flex-col items-center gap-[4px]">
+              <Flex align="center" justify="center" gap={16} className="my-[16px]">
+                <Flex vertical align="center" gap={4}>
                   <Avatar className="bg-[#ffebb8] text-[#693f16] font-semibold" gap={5}>
                     10+
                   </Avatar>
                   <span className="text-[12px] text-[#D49753]">10+ 连接协议</span>
-                </div>
-                <div className="flex flex-col items-center gap-[4px]">
+                </Flex>
+                <Flex vertical align="center" gap={4}>
                   <Avatar className="bg-[#ffebb8]" icon={<IconFont type="icon-customize" />} />
                   <span className="text-[12px] text-[#D49753]">个性化定制</span>
-                </div>
-                <div className="flex flex-col items-center gap-[4px]">
+                </Flex>
+                <Flex vertical align="center" gap={4}>
                   <Avatar className="bg-[#ffebb8]" icon={<IconFont type="icon-advanced" />} />
                   <span className="text-[12px] text-[#D49753]">更多高级功能</span>
-                </div>
-              </div>
+                </Flex>
+              </Flex>
               <a
                 className="gradient-gold-bg block rounded-[2px] text-center py-[8px] px-[10px] text-[#693f16] cursor-pointer hover:text-[#693f16]"
                 href="https://www.hootrhino.com/pricing"
@@ -85,11 +92,13 @@ const HeaderDropdown: React.FC<HeaderDropdownProps> = ({ overlayClassName: cls, 
             </div>
           </div>
           <div
-            className="rounded-[8px] mb-[10px] mx-[10px]"
+            className="rounded-[8px] mb-[24px] mx-[14px]"
             style={{ border: '1px solid rgba(5,5,5,0.06)' }}
           >
-            <div
-              className="flex items-center justify-between m-[10px] cursor-pointer"
+            <Flex
+              align="center"
+              justify="space-between"
+              className="m-[10px] cursor-pointer"
               onClick={() => {
                 history.push('/system');
                 setActiveKey('user');
@@ -97,13 +106,37 @@ const HeaderDropdown: React.FC<HeaderDropdownProps> = ({ overlayClassName: cls, 
             >
               <span>修改账户信息</span>
               <RightOutlined />
-            </div>
+            </Flex>
           </div>
-          {React.cloneElement(menu as React.ReactElement, { style: menuStyle })}
+          <Flex
+            align="center"
+            justify="space-between"
+            className="p-[10px] bg-[rgba(31,35,41,0.08)] cursor-pointer"
+            onClick={loginOut}
+          >
+            <div>{formatMessage({ id: 'component.button.logout' })}</div>
+            <LogoutOutlined />
+          </Flex>
         </div>
-      )}
-      {...restProps}
-    />
+      }
+      {...props}
+    >
+      <Flex align="center" className="h-[56px] ml-auto px-[12px] cursor-pointer rounded-[2px]">
+        <Flex align="center" className="h-[40px] px-[12px] gradient-border rounded-[2px]">
+          <Avatar
+            size="small"
+            className="text-[#fff] bg-[#e0eafc] mr-[8px] align-top md:m-0"
+            src={undefined}
+            icon={<UserOutlined />}
+            alt="avatar"
+          />
+          <Space size={4} className="gradient-text">
+            <IconFont type="icon-pro" />
+            <span className="text-[12px]">升级企业版，解锁专业效能</span>
+          </Space>
+        </Flex>
+      </Flex>
+    </Popover>
   );
 };
 
--- src/components/RightContent/AvatarDropdown.tsx
+++ src/components/RightContent/AvatarDropdown.tsx
@@ -1,9 +1,5 @@
-import { IconFont } from '@/utils/utils';
-import { ExclamationCircleOutlined, LogoutOutlined, UserOutlined } from '@ant-design/icons';
-import { useIntl, useModel } from '@umijs/max';
-import { App, Avatar, Spin } from 'antd';
-import type { MenuInfo } from 'rc-menu/es/interface';
-import React, { useCallback } from 'react';
+import { useModel } from '@umijs/max';
+import { Spin } from 'antd';
 import HeaderDropdown from '../HeaderDropdown';
 
 export type GlobalHeaderRightProps = {
@@ -11,34 +7,8 @@ export type GlobalHeaderRightProps = {
 };
 
 const AvatarDropdown: React.FC<GlobalHeaderRightProps> = () => {
-  const { logout } = useModel('useUser');
   const { initialState } = useModel('@@initialState');
-  const { formatMessage } = useIntl();
   const { currentUser } = initialState || {};
-
-  const { modal } = App.useApp();
-  /**
-   * 退出登录，并且将当前的 url 保存
-   */
-  const loginOut = async () => {
-    modal.confirm({
-      title: formatMessage({ id: 'modal.content.logout' }),
-      icon: <ExclamationCircleOutlined />,
-      onOk: logout,
-      okText: formatMessage({ id: 'button.ok' }),
-      cancelText: formatMessage({ id: 'button.cancel' }),
-    });
-  };
-
-  const onMenuClick = useCallback(
-    (event: MenuInfo) => {
-      const { key } = event;
-      if (key === 'logout') {
-        loginOut();
-      }
-    },
-    [loginOut],
-  );
 
   if (!initialState || !currentUser || !currentUser.username) {
     // loading
@@ -49,40 +19,7 @@ const AvatarDropdown: React.FC<GlobalHeaderRightProps> = () => {
     );
   }
 
-  const menuItems = [
-    {
-      key: 'logout',
-      icon: <LogoutOutlined />,
-      label: formatMessage({ id: 'component.button.logout' }),
-    },
-  ];
-
-  return (
-    <HeaderDropdown
-      menu={{
-        selectedKeys: [],
-        onClick: onMenuClick,
-        items: menuItems,
-      }}
-      overlayClassName="w-[300px] user-dropdown"
-    >
-      <div className="flex items-center h-[56px] ml-auto px-[12px] cursor-pointer rounded-[2px]">
-        <div className="flex items-center h-[40px] px-[12px] gradient-border rounded-[2px]">
-          <Avatar
-            size="small"
-            className="text-[#fff] bg-[#e0eafc] mr-[8px] align-top md:m-0"
-            src={undefined}
-            icon={<UserOutlined />}
-            alt="avatar"
-          />
-          <div className="text-[12px] gradient-text">
-            <IconFont type="icon-pro" className="pr-[6px]" />
-            升级企业版，解锁专业效能
-          </div>
-        </div>
-      </div>
-    </HeaderDropdown>
-  );
+  return <HeaderDropdown />;
 };
 
 export default AvatarDropdown;
